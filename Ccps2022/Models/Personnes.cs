3068de97ab3bc949e521219eff579b45571752df
Terogene Claudio <cterogene@gmail.com>
2022-01-31 20:04:12 -0800

    Updating the Ui and Creating the personnes classs as a model

---
+++ Ccps2022/Models/Personnes.cs
@@ -0,0 +1,17 @@
+﻿namespace Ccps2022.Models
+{
+    public class Personnes
+    {
+        public string prenom { get; set; }
+        public string nom { get; set; }
+        public int telephone { get; set; }
+        public DateTime Ddn { get; set; }
+        public string Adresse { get; set; }
+        public string pays { get; set; }
+        public string Email { get; set; }
+        public string sexe { get; set; }
+        public int etudiant { get; set; }
+        public int professeur { get; set; }
+        public int adminStaff { get; set; }
+    }
+}
